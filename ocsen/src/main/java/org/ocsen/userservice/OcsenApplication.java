9de7a1e58ba2ee1e1426b5e392ba8815cb81e74c
Do Lam Binh Minh <ocsen.hoc.code@gmail.com>
2019-10-18 14:05:00 +0700

    first commit

---
+++ ocsen/src/main/java/org/ocsen/userservice/OcsenApplication.java
@@ -0,0 +1,13 @@
+package org.ocsen.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcsenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcsenApplication.class, args);
+	}
+
+}
